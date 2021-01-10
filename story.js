7d0eff46072340dedd5185c95f840b0b1043d7b5
hajarghannami <hajarghannami@outlook.com>
2021-01-10 13:07:40 +0300

    here's the task :))

---
+++ story.js
@@ -0,0 +1,6 @@
+const firstname = "Hajar"; 
+let age = "24"; 
+let hobby = "art"; 
+
+console.log(`Mrs. ${firstname}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
+
